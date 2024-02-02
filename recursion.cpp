87227469e633a973358ba827567d67dbd40b3095
Dhurba <amrit20190@gmail.com>
2024-02-02 09:22:50 +0545

    first commit

---
+++ recursion.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdbool.h>
+int fibo(int n)
+{
+    if (n==1)
+    {
+        return 0;
+    }
+    else if(n==2)
+    {
+        return 1;
+    }
+    else
+    {
+    return((fibo(n-1)+fibo(n-2)));  
+    }
+}
+int main()
+{
+    int n;
+    printf("Enter the number:\n");
+    scanf("%d",&n);
+    printf("The %d th fibonaaci number is:%d",n,fibo(n));
+    return 0;
+}
